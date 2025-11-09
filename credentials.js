d4739773082e1da3c12a8bac11ccdd8e8686bd17
Adeola Adefala <163596973+Adeola6388@users.noreply.github.com>
2025-11-08 19:28:09 -0500

    Add files via upload

--- credentials.js
+++ credentials.js
@@ -1,4 +1,13 @@
 export const urls = {
-    home: 'https://mini-bank.testamplify.com/',
-    login: 'https://mini-bank.testamplify.com/login'
+  home: 'https://mini-bank.testamplify.com/',
+  login: 'https://mini-bank.testamplify.com/login',
+  dashboard: 'https://mini-bank.testamplify.com/dashboard'
+};
+
+export const credentials = {
+  valid: { email: 'adeola.adefala@gmail.com', password: 'A4076072328a!'},
+  invalid: [
+    { email: 'wrong1@example.com', password: 'badpass' },
+    { email: 'wrong2@example.com', password: 'badpass' }
+  ]
 };
